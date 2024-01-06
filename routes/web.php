1c48eeb680283af2b55294c76b2c589098926828
sleepysiemens <2001sema@gmail.com>
2024-01-06 18:17:01 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/test', 'App\Http\Controllers\MainController@index')->name('main.index');
+Route::get('/', 'App\Http\Controllers\MainController@index')->name('main.index');
