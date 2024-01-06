898cb071f394b91a90576254d7b5634a0fe1dd9c
sleepysiemens <2001sema@gmail.com>
2024-01-06 18:57:52 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/appz.css', 'resources/js/app.js'],
+            input: ['resources/css/app.min.css', 'resources/css/vendor.min.css', 'resources/js/app.min.js', 'resources/js/vendor.min.js'],
             refresh: true,
         }),
     ],
