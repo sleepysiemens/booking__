208a57703eec3ede66534f4a59fa2bcbeb47c578
sleepysiemens <2001sema@gmail.com>
2024-01-13 23:02:49 +0700

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -14,8 +14,8 @@ class TestController extends Controller
 
         $flightService = $travel->getFlightService();
         $flightService
-            ->setIp('77.222.38.12')
-            ->setHost('localhost')
+            ->setIp('212.164.38.195')
+            ->setHost('77.222.38.12')
             ->setMarker('36076')
             ->addPassenger('adults', 2)
             ->addSegment('LED', 'MOW', '2016-02-01');
