d43ed30ec6d0bd4bf2591f239635617129eb27f6
sleepysiemens <2001sema@gmail.com>
2024-01-13 23:08:17 +0700

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -18,7 +18,7 @@ class TestController extends Controller
             ->setHost('77.222.38.12')
             ->setMarker('36076')
             ->addPassenger('adults', 2)
-            ->addSegment('LED', 'MOW', '2016-02-01');
+            ->addSegment('LED', 'MOW', '2024-02-02');
         dump($flightService);
         $searchData    = $flightService->search('ru', 'Y');
         $searchResults = $flightService->getSearchResults($searchData['search_id']);
