d18ffd6a4fe08ca7ea056aa2121f076283459761
sleepysiemens <2001sema@gmail.com>
2024-01-24 00:42:07 +0700

    smart search second commit

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -17,12 +17,52 @@ class TestController extends Controller
 
     public function test()
     {
-        $result=$this->OldSeachService->get_search_link();
+        //$result=$this->OldSeachService->get_search_link();
 
         //$client = new Client();
         //$response = $client->get("https://api.travelpayouts.com/v1/prices/cheap?origin={$origin}&destination={$destination}&depart_date={$departureDate}&token={$this->apiKey}");
 
-        dd($result);
+        //dd($result);
+
+        $url = 'https://api.travelpayouts.com/v1/flight_search';
+
+        $params =
+            [
+                "signature"=> "b97bad81ba0f1f9f9994f0291c1e3123",
+                "marker"=> "36076",
+                "host"=> "tripavia.com",
+                "user_ip"=> "212.164.65.246",
+                "locale"=> "ru",
+                "trip_class"=> "Y",
+
+                "passengers"=>
+                [
+                    "adults"=> 1,
+                    "children"=> 0,
+                    "infants"=> 0
+                ],
+                "segments"=>
+                [
+                    [
+                        "origin"=> "LED",
+                        "destination"=> "MOW",
+                        "date"=> "2024-02-02"
+                    ]
+                ]
+            ];
+
+        $headers = [
+            //'Content-Type' => 'application/json',  // Уточните тип содержимого, если необходимо
+            'x-access-token' => '048a44328dd6efc65b762b8e8c20e30a',  // Если требуется авторизация
+        ];
+
+        $response = Http::withHeaders($headers)->post($url, $params);
+
+        $status = $response->status();
+        $data = $response->json();
+
+// Вывод результата
+        dd($status, $data);
     }
 
 }
