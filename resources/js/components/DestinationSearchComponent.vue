5536bbab06b0c0155f2c8545ccfc26dc1034b6c6
sleepysiemens <2001sema@gmail.com>
2024-01-26 21:55:19 +0700

    убрал автокомплит

--- resources/js/components/DestinationSearchComponent.vue
+++ resources/js/components/DestinationSearchComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <fieldset class="first_input brdr-b-l p-0 col-1 h-60px m-0 col-lg col-6 position-relative d-flex">
         <legend style="all: revert;" class="fs-12px ms-3 opacity-70">Куда</legend>
-        <input class="bg-transparent border-0 ms-3 p-0 h-100" v-model="searchQuery" @input="search" @click="showCard" @blur="hideCard" type="text" name="destination"/>
+        <input class="bg-transparent border-0 ms-3 p-0 h-100" v-model="searchQuery" @input="search" @click="showCard" @blur="hideCard" type="text" name="destination" autocomplete="off"/>
         <input type="hidden" name="destination_" v-model="searchQuery_">
         <div class="card position-absolute top-100 mt-3 search-origin-component overflow-y-scroll z-3" v-show="isCardVisible">
             <ul class="list-group border-0">
