5deb39f2824c3d151bd331636036b44112fe43a6
sleepysiemens <2001sema@gmail.com>
2024-01-27 00:15:17 +0700

    оптимизировал загрузку страницы

--- resources/js/components/DestinationSearchComponent.vue
+++ resources/js/components/DestinationSearchComponent.vue
@@ -38,8 +38,8 @@
 export default {
     data() {
         return {
-            searchQuery: window.requestData['origin'],
-            searchQuery_: window.requestData['origin_'],
+            searchQuery: window.requestData['destination'],
+            searchQuery_: window.requestData['destination_'],
             searchResults: [],
             isCardVisible: false,
             selectedResult: '',
