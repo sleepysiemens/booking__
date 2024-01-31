92002bea2b792287f803e714cf86816163c688b6
siemens <2001sema@gmail.com>
2024-02-01 05:47:51 +0700

    тест поиска 2

--- app/Services/FlightSearchService.php
+++ app/Services/FlightSearchService.php
@@ -3,81 +3,96 @@
 
 namespace App\Services;
 
-use Symfony\Component\Panther\PantherTestCase;
-use Symfony\Component\Panther\PantherTestCase as PantherTestCaseAlias;
+use Symfony\Component\Panther\Client;
 
-class FlightSearchService extends PantherTestCase
+class FlightSearchService
 {
     public function parseFlightInfo($origin, $destination, $depart_date)
     {
         // Создаем клиента Panther
-        PantherTestCaseAlias::startWebServer();
-        $client = PantherTestCaseAlias::createPantherClient();
+        $client = Client::createChromeClient();
 
-        $depart_date = date('dm', strtotime($depart_date));
-        $url = 'https://www.onetwotrip.com/ru/f/search/' . $depart_date . $origin . $destination . '?sc=E&ac=1&srcmarker2=newindex';
+        $depart_date=date('dm',strtotime($depart_date));
 
-        $crawler = $client->request('GET', $url);
+        //$crawler = $client->request('GET', 'https://www.onetwotrip.com/ru/f/search/'.$depart_date.$origin.$destination.'?sc=E&ac=1&srcmarker2=newindex');
+        $crawler = $client->request('GET', 'https://www.onetwotrip.com/ru/f/search/0202LEDMOW?sc=E&ac=1&srcmarker2=newindex');
 
         // Подождите некоторое время, чтобы данные подгрузились
         $client->waitFor('.Vo739'); // Увеличьте время ожидания при необходимости
 
         // Теперь можно собирать данные, которые подгрузились в результате скролла
-        $tickets = $crawler->filter('.Vo739')->each(function ($node) {
+        $tickets = $crawler->filter('.Vo739')->each(function ($node)
+        {
             $depart_time = $node->filter('.QPp8j')->first()
-                ->each(function ($child) {
+                ->each(function ($child)
+                {
                     return $child->text('', true);
                 });
+            //
             $arrival_time = $node->filter('.QPp8j')->last()
-                ->each(function ($child) {
+                ->each(function ($child)
+                {
                     return $child->text('', true);
                 });
-            $airline = $node->filter('._7kfLX')
-                ->each(function ($child) {
+            //
+            $airline=$node->filter('._7kfLX')
+                ->each(function ($child)
+                {
                     return $child->text('', true);
                 });
-            $duration = $node->filter('.mBsCn')->filter('span')->first()
-                ->each(function ($child) {
+            //
+            $duration=$node->filter('.mBsCn')->filter('span')->first()
+                ->each(function ($child)
+                {
                     return $child->text('', true);
                 });
-            $airline_logo = $node->filter('.NJzsX')
-                ->each(function ($child) {
+            //
+            $airline_logo=$node->filter('.NJzsX')
+                ->each(function ($child)
+                {
                     return $child->getAttribute('background-image');
                 });
-            $price = $node->filter('._4-iO8')->filter('span')->first()
-                ->each(function ($child) {
+            //
+            $price=$node->filter('._4-iO8')->filter('span')->first()
+                ->each(function ($child)
+                {
                     return $child->text('', true);
                 });
-            $origin = $node->filter('.QVkKQ')->first()
-                ->each(function ($child) {
+            //
+            $origin=$node->filter('.QVkKQ')->first()
+                ->each(function ($child)
+                {
                     return $child->text('', true);
                 });
-            $destination = $node->filter('.QVkKQ')->last()
-                ->each(function ($child) {
+            //
+            $destination=$node->filter('.QVkKQ')->last()
+                ->each(function ($child)
+                {
                     return $child->text('', true);
                 });
 
-            if ($depart_time != NULL && $arrival_time != NULL) {
-                $ticket = [
-                    'depart_time' => implode(' ', $depart_time),
-                    'arrival_time' => implode(' ', $arrival_time),
-                    'airline' => implode(' ', $airline),
-                    'airline_logo' => implode(' ', $airline_logo),
-                    'duration' => implode(' ', $duration),
-                    'price' => implode(' ', $price),
-                    'origin' => implode(' ', $origin),
-                    'destination' => implode(' ', $destination),
-                ];
+            if($depart_time!=NULL && $arrival_time!=NULL) {
+                $ticket =
+                    [
+                        'depart_time' => implode(' ', $depart_time),
+                        'arrival_time' => implode(' ', $arrival_time),
+                        'airline' => implode(' ', $airline),
+                        'airline_logo' => implode(' ', $airline_logo),
+                        'duration' => implode(' ', $duration),
+                        'price' => implode(' ', $price),
+                        'origin' => implode(' ', $origin),
+                        'destination' => implode(' ', $destination),
+                    ];
                 return $ticket;
             }
             return [];
         });
 
-        PantherTestCaseAlias::stopWebServer();
-
-        $i = 0;
-        foreach ($tickets as $ticket) {
-            if ($ticket == NULL) {
+        $i=0;
+        foreach ($tickets as $ticket)
+        {
+            if($ticket==NULL)
+            {
                 unset($tickets[$i]);
             }
             $i++;
@@ -87,12 +102,14 @@ class FlightSearchService extends PantherTestCase
 
     public function FilterAirlines($tickets)
     {
-        $airlines = [];
-        foreach ($tickets as $ticket) {
-            $airlines[] = [
-                'airline' => $ticket['airline'],
-                'logo' => $ticket['airline_logo'],
-            ];
+        $airlines=[];
+        foreach ($tickets as $ticket)
+        {
+            $airlines[]=
+                [
+                    'airline'=>$ticket['airline'],
+                    'logo'=>$ticket['airline_logo'],
+                ];
         }
 
         return $airlines;
