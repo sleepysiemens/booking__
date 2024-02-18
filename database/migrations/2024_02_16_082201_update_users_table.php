0d3c9df6375b06ed9d6a284670b199259cc13eb0
siemens <2001sema@gmail.com>
2024-02-18 21:34:00 +0700

    Партнерская программа

--- database/migrations/2024_02_16_082201_update_users_table.php
+++ database/migrations/2024_02_16_082201_update_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function ($table)
         {
-            $table->boolean('is_partner')->defaut(false);
+            $table->boolean('is_partner')->defaut(0);
             $table->integer('ref_id')->nullable();
         });
     }
