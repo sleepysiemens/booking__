93eac8ee73ea98ab3e0fe34ca6efbafe825519f8
siemens <2001sema@gmail.com>
2024-02-21 21:56:15 +0700

    tg

--- app/Jobs/UpdateOrderStatus.php
+++ app/Jobs/UpdateOrderStatus.php
@@ -39,24 +39,33 @@ class UpdateOrderStatus implements ShouldQueue
     {
         if ($this->order->created_at->addSeconds(1)->isPast())
         {
-            //$this->order->update(['is_confirmed'=>true]);
+            $this->order->update(['is_confirmed'=>true]);
             $user=User::query()->where('id','=', $this->order->user_id)->first();
 
             if($user->tg_chat_id!=null)
             {
                 $chat=TelegraphChat::query()->where('chat_id','=',$user->tg_chat_id)->first();
-                $message=Telegraph::message('Заказ №'.$this->order->id.' подтвержден')->keyboard(
+
+                /*$message=Telegraph::message('Заказ №'.$this->order->id.' подтвержден')->keyboard(
                     Keyboard::make()->buttons(
                         [
                             Button::make('Посмотреть билет')->url(route('ticket.index',$this->order)),
                             Button::make('Скачать билет')->url(route('ticket.download',$this->order)),
                         ]
                     )
-                )->chat($chat)->send();
-//                $chat->markdown('Заказ №'.$this->order->id.' подтвержден')->send();
+                )->chat($user->tg_chat_id)->send();
+                $chat->markdown('Заказ №'.$this->order->id.' подтвержден')->send();*/
+
+                $chat->message('Заказ №'.$this->order->id.' подтвержден')->keyboard(
+                    Keyboard::make()->buttons(
+                        [
+                            Button::make('Посмотреть билет')->url(route('ticket.index',$this->order)),
+                            Button::make('Скачать билет')->url(route('ticket.download',$this->order)),
+                        ]
+                    )
+                )->send();
 
             }
-
 
             Log::info('UpdateBookingStatus выполнен');
         }
