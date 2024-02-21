f0b1cfc11d0940767e87295fd7acefdcae8785ed
siemens <2001sema@gmail.com>
2024-02-21 17:14:50 +0700

    tg

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Jobs\UpdateOrderStatus;
-use App\Models\Order;
+
 use App\Models\PartnershipApplication;
-use DefStudio\Telegraph\Models\TelegraphChat;
-use Illuminate\Http\Request;
 use App\Services\AirportService;
 use App\Models\BlogPost;
 use App\Models\Review;
@@ -17,10 +14,6 @@ class MainController extends Controller
 {
     public function index()
     {
-        $order=Order::query()->latest()->first();
-        UpdateOrderStatus::dispatch($order)->delay(now()->addSeconds(2));
-
-
         $airportService = new AirportService();
         $airports = $airportService->getAllAirports();
 
