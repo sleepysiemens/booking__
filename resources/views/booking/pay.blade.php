b34efef2be3e607084551b218fb1321ce26d3f81
siemens <2001sema@gmail.com>
2024-02-23 12:40:45 +0700

    tg

--- resources/views/booking/pay.blade.php
+++ resources/views/booking/pay.blade.php
@@ -45,7 +45,7 @@
 
                         <div class="col-6">
                             <p class="opacity-50 fs-13px mb-0">{{__('Заказчик')}}</p>
-                            <p class="fs-16px mt-1">{{}}</p>
+                            <p class="fs-16px mt-1">{{----}}</p>
                         </div>
                         <div class="col-6">
                             <p class="opacity-50 fs-13px mb-0">{{__('Сумма заказа')}}</p>
