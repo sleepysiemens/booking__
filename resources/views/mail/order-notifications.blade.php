cfe1df9efa5d950868f134dc549a93cd0dc8243b
siemens <2001sema@gmail.com>
2024-02-24 12:17:10 +0700

    tg

--- resources/views/mail/order-notifications.blade.php
+++ resources/views/mail/order-notifications.blade.php
@@ -1,3 +1,5 @@
 <x-mail::message>
-Статус вашего заказа #{{$order}} изменился
+Заказ #{{$order}} подтвержден. <br>
+    <a href="{{route('ticket.index',$order)}}">Посмотреть билет</a>
+    <a href="{{route('ticket.download',$order)}}">Скачать билет</a>
 </x-mail::message>
